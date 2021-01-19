3e4970ce940613d7ead906616dab2b91cc23f6cd
MutiCodes <m.etterphoya@gmail.com>
2021-01-19 21:14:58 +0200

    Create 1.syn_var_num_exercise.py
    Responded to a simple programme calculating 'area'

---
+++ 1.syn_var_num_exercise.py
@@ -0,0 +1,8 @@
+pi = 3.14159 #Approximate
+diameter = 3
+# Create a variable called 'radius' equal to half the diameter
+radius = diameter * 0.5
+
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+area = pi*(radius**2)
+print(area)
