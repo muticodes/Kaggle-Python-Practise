b9c0c661bab96bfec802d5cd05760b7e750b1f0e
MutiCodes <m.etterphoya@gmail.com>
2021-01-20 21:54:09 +0200

    Create bool_exercise.py
    Created a function called sign which takes a numerical argument and returns -1 if it's negative, 1 if it's positive, and 0 if it's 0.

---
+++ bool_exercise.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 20:50:43 2021
+
+# Define a function called sign which takes a numerical argument 
+# and returns -1 if it's negative, 1 if it's positive, and 0 if it's 0.
+
+def sign(num):
+    if num < 0:
+        return(-1)
+    elif num > 0:
+        return(1)
+    else:
+        return(0)
