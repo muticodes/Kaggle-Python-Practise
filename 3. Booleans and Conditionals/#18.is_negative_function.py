fecefab27475f3503d060511533c2e030683e5ab
MutiCodes <m.etterphoya@gmail.com>
2021-01-25 08:44:45 +0200

    Create #18.is_negative_function.py

---
+++ 3. Booleans and Conditionals/#18.is_negative_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 13:26:59 2021
+
+@author: Colin Moris from Kaggle
+
+"""
+def is_negative(number):
+    if number < 0:
+        return True
+    else:
+        return False
+
+print(is_negative(9))
+
+
