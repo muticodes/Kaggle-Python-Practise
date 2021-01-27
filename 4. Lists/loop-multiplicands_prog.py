1bfec7d89b7624425581c5fe01cff0922f48d61d
MutiCodes <m.etterphoya@gmail.com>
2021-01-27 05:32:22 +0200

    Create loop-multiplicands_prog.py
    Copied Sample 'for loop' program

---
+++ 4. Lists/loop-multiplicands_prog.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 05:30:39 2021
+
+@author: Colin Moris
+"""
+#Sample for loop program
+
+multiplicands = (2, 2, 2, 3, 3, 5)
+product = 1
+for mult in multiplicands:
+    product = product * mult
+print(product)
