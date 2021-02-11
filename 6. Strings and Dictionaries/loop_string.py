87a8a5359f8934a8d1abc7306213df1e1e71547c
MutiCodes <m.etterphoya@gmail.com>
2021-02-11 09:26:04 +0200

    Looping a string
    Looping a string

---
+++ 6. Strings and Dictionaries/loop_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 07:08:19 2021
+
+@author: mutimw
+"""
+
+planet = 'Pluto'
+loud_planet = [char+'! ' for char in planet]
+print(loud_planet)
+
